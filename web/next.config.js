2856e65b49b4bdaa3ae977c83efd0edca71894e4
sahil <sauryahjoshi@gmail.com>
2026-09-11 13:59:39 +0530

    chore(web): disable Next.js development indicator badge in next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: false,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8085',
   },
